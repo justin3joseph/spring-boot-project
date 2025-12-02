529f1829ceb1fc986d081ba164bcb9cb51400a4f
justin3joseph <justin3joseph@gmail.com>
2025-12-02 17:55:38 +0530

    Initial commit

---
+++ src/main/java/com/example/devops_demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.devops_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Spring Boot CI/CD Pipeline Works!";
+    }
+}
